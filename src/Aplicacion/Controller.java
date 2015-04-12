a0684589f85b5c23818ee82b12065a76f44f258d
Marcos <marketes94@gmail.com>
2015-04-12 20:57:22 +0200

    Corregido un pequeño detalle del método de listar

--- src/Aplicacion/Controller.java
+++ src/Aplicacion/Controller.java
@@ -4,12 +4,11 @@ import Database.JDBCTemplate;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Map;
 
 /**
- * Controlador de la aplicaciÛn
+ * Controlador de la aplicaci√≥n
  */
 public class Controller {
     
@@ -33,35 +32,35 @@ public class Controller {
     
     /**
      * Accede al listado de coches
-     * @param form Filtro de par·metros
+     * @param form Filtro de par√°metros
      */
     public static void list(Form form){
     	JDBCTemplate template = JDBCTemplate.getJDBCTemplate();
-    	String options=getSQLOptions(form);
-    	String query="SELECT * FROM Car "+options;
-    	ResultSet rs =template.executeQuery(query).getResultSet();
-    	CarModel car = null;
-    	CarModel[]models = null;
+    	String options = getSQLOptions(form);
+    	String query = "SELECT * FROM Car "+options;
+    	ResultSet rs = template.executeQuery(query).getResultSet();
+    	CarModel car;
+    	CarModel[] models = null;
     	int sizeVector=0;
     	try {
-    		rs.first();
-    		//calculamos el tamano que tendra el vector de modelos
-			while(rs.next()){
-				sizeVector++;
-			}
-    		models=new CarModel[sizeVector];
-			rs.first();
-			//introducimos cada coche en el vector
-			for(int i=0;i<sizeVector;i++){
-				rs.next();
-				car = new CarModel(rs.getString("name"), rs.getString("fuel_type"), rs.getInt("power"), rs.getString("category"),
-	                    rs.getInt("number_doors"), rs.getInt("cost"), rs.getDouble("consumption"), rs.getInt("number_seats"));
-				models[i]=car;
-			}
-		} catch (SQLException e) {}
+            rs.first();
+            //calculamos el tama√±o que tendr√° el vector de modelos
+            while(rs.next()){
+                sizeVector++;
+            }
+            models=new CarModel[sizeVector];
+            rs.first();
+            //introducimos cada coche en el vector
+            for(int i=0; i<sizeVector; i++){
+                car = new CarModel(rs.getString("name"), rs.getString("fuel_type"), rs.getInt("power"), rs.getString("category"),
+                        rs.getInt("number_doors"), rs.getInt("cost"), rs.getDouble("consumption"), rs.getInt("number_seats"));
+                models[i] = car;
+                rs.next();
+            }
+        } catch (SQLException e){}
+        
     	//pasamos el vector de modelos a la vista
     	View.list(models);
-    	
     }
     
     /**
@@ -72,7 +71,7 @@ public class Controller {
         JDBCTemplate template = JDBCTemplate.getJDBCTemplate();
         
         String query = "SELECT * FROM Car WHERE name='"+carName+"'";
-        //la query sÛlo devolver· una fila como resultado
+        //la query s√≥lo devolver√° una fila como resultado
         ResultSet rs = template.executeQuery(query).getResultSet();
         CarModel car = null;
         try{
@@ -85,7 +84,7 @@ public class Controller {
     }
     
     /**
-     * Accede a la pantalla de ediciÛn de un coche
+     * Accede a la pantalla de edici√≥n de un coche
      * @param carName Nombre identificador del coche
      */
     public static void edit(String carName){
@@ -93,13 +92,13 @@ public class Controller {
     }
     
     /**
-     * EnvÌa una solicitud de contacto para concertar una cita y ver un coche
-     * @param form Filtro de par·metros
+     * Env√≠a una solicitud de contacto para concertar una cita y ver un coche
+     * @param form Filtro de par√°metros
      * @param carName Nombre identificador del coche que desea ver el cliente
      */
     public static void contactRequest(Form form, String carName){
-        String insert="INSERT into Request (request_id,requester_name, requester_mail, car_name, request_date) "+
-        		"VALUES ( requests_id.nextval, ";
+        JDBCTemplate template = JDBCTemplate.getJDBCTemplate();
+        String insert="INSERT INTO Request VALUES (requests_id.nextval, ";
         Map.Entry<String, String> aux;
         Iterator<Map.Entry<String, String>> iterator = form.getIterator();
         insert+=iterator.next();
@@ -110,7 +109,6 @@ public class Controller {
         }
         //momento de la compra
         insert+=carName+", CURRENT_STAMP);";
-        JDBCTemplate template = JDBCTemplate.getJDBCTemplate();
         template.executeQuery(insert);
     }
 }
