3060aaa8057f6468bad0cbdfa6ce1507deb6792a
AlejandroDiesteCortes <alej.14@hotmail.com>
2015-04-25 21:40:48 +0200

    Añadida la funcion de borrar un modelo al Administrador. Controller.java MainWindow.form MainWindow.java View.java

--- src/Admin/Controller.java
+++ src/Admin/Controller.java
@@ -13,73 +13,83 @@ import javax.swing.JTable;
  * Controlador de la aplicación
  */
 public class Controller {
-    
+
     /**
-     * Obtiene un string que representa en SQL una condición para un
-     * determinado par clave => valor
+     * Obtiene un string que representa en SQL una condición para un determinado
+     * par clave => valor
+     *
      * @return String que representa una condición en SQL
      */
-    private static String getSQLOption(String key, String value){
-        if (key.endsWith("Min")) return key.substring(0, key.lastIndexOf("Min")) + ">='" + value + "'";
-        else if (key.endsWith("Max")) return key.substring(0, key.lastIndexOf("Max")) + "<='" + value + "'";
-        //NOTA: no hay problemas con que sea un tipo de dato distinto a string
-        else return key + "='" + value + "'";
+    private static String getSQLOption(String key, String value) {
+        if (key.endsWith("Min")) {
+            return key.substring(0, key.lastIndexOf("Min")) + ">='" + value + "'";
+        } else if (key.endsWith("Max")) {
+            return key.substring(0, key.lastIndexOf("Max")) + "<='" + value + "'";
+        } //NOTA: no hay problemas con que sea un tipo de dato distinto a string
+        else {
+            return key + "='" + value + "'";
+        }
     }
-    
+
     /**
-     * Obtiene un string que representa en SQL un conjunto de condiciones
-     * para determinados pares clave => valor
+     * Obtiene un string que representa en SQL un conjunto de condiciones para
+     * determinados pares clave => valor
+     *
      * @return String que representa un conjunto de condiciones en SQL
      */
-    private static String getSQLOptions(Form form){
-        if (form.isEmpty()) return "";
-        else{
+    private static String getSQLOptions(Form form) {
+        if (form.isEmpty()) {
+            return "";
+        } else {
             String res = "WHERE ";
             Map.Entry<String, String> aux;
-            
+
             //condiciones de la forma campo='valor'
             Iterator<Map.Entry<String, String>> iterator = form.getIterator();
             aux = iterator.next();
             res += getSQLOption(aux.getKey(), aux.getValue());
-            while (iterator.hasNext()){
+            while (iterator.hasNext()) {
                 aux = iterator.next();
                 res += " AND " + getSQLOption(aux.getKey(), aux.getValue());
             }
-            
+
             return res;
         }
     }
-    
+
     /**
      * Accede al listado de coches
+     *
      * @param form Filtro de parámetros
      * @param modelsList Lista que contiene los resultados de la búsqueda
      */
-    public static void list(Form form, JTable modelsList){
-    	JDBCTemplate template = JDBCTemplate.getJDBCTemplate();
-    	String options = getSQLOptions(form);
-    	String query = "SELECT * FROM Car "+options;
-    	ResultSet rs = template.executeQuery(query).getResultSet();
+    public static void list(Form form, JTable modelsList) {
+        JDBCTemplate template = JDBCTemplate.getJDBCTemplate();
+        String options = getSQLOptions(form);
+        String query = "SELECT * FROM Car " + options;
+        ResultSet rs = template.executeQuery(query).getResultSet();
         ArrayList<CarModel> models = new ArrayList<CarModel>();
-    	try {
-            while(rs.next()){
+        try {
+            while (rs.next()) {
                 models.add(new CarModel(rs.getString("name"), rs.getString("fuel_type"), rs.getInt("power"), rs.getString("category"),
                         rs.getInt("number_doors"), rs.getInt("cost"), rs.getDouble("consumption"), rs.getInt("number_seats")));
             }
-        } catch (SQLException e){}
-        
-    	//pasamos el vector de modelos a la vista
-    	View.list(models.toArray(new CarModel[models.size()]), modelsList);
+        } catch (SQLException e) {
+        }
+
+        //pasamos el vector de modelos a la vista
+        View.list(models.toArray(new CarModel[models.size()]), modelsList);
     }
-    
+
     /**
      * Envía una solicitud de contacto para concertar una cita y ver un coche
+     *
      * @param form Filtro de parámetros
      * @param carName Nombre identificador del coche que desea ver el cliente
      */
-    public static void contactRequest(Form form, String carName){
+    public static void contactRequest(Form form, String carName) {
         JDBCTemplate template = JDBCTemplate.getJDBCTemplate();
-        String insert="INSERT INTO Requests VALUES (requests_id.nextval, ";
+        String insert = "INSERT INTO Requests VALUES (requests_id.nextval, ";
         insert += "'" + form.getParam("name") + " " + form.getParam("surname") + "'";
         insert += ", '" + form.getParam("mail") + "'";
         insert += ", '" + carName + "'";
@@ -87,5 +97,20 @@ public class Controller {
         insert += ", CURRENT_TIMESTAMP";
         insert += ", '" + form.getParam("city") + "')";
         template.executeQuery(insert);
+    }
+
+    /**
+     * Borra el modelo pasado por parámetro de la base de datos
+     *
+     * @param model Nombre del modelo
+     */
+    public static void borrar(String model) {
+        JDBCTemplate template = JDBCTemplate.getJDBCTemplate();
+        //primero se borran las request de ese modelo
+        String deleteRequests = "DELETE FROM Requests WHERE car_name ='" + model + "'";
+        template.executeSentence(deleteRequests);
+        //despues se borra el modelo concreto
+        String delete = "DELETE FROM Car WHERE name ='" + model + "'";
+        template.executeSentence(delete);
     }
 }
--- src/Admin/View.java
+++ src/Admin/View.java
@@ -12,33 +12,47 @@ import javax.swing.table.DefaultTableModel;
  * Vista de la aplicación
  */
 public class View {
-    
+
     /**
      * Visualiza una lista de coches
+     *
      * @param models Lista de coches
      * @param modelsList Lista que contiene los resultados de la búsqueda
      */
-    public static void list(CarModel[] models, JTable modelsList){
+    public static void list(CarModel[] models, JTable modelsList) {
         final DefaultTableModel tableModel = (DefaultTableModel) modelsList.getModel();
         //limpia la lista
         tableModel.setRowCount(0);
-        
+
         //actualiza la tabla con nuevos coches
-        for (final CarModel c : models){
-            tableModel.addRow(new Object[]{c.getName(), c.getCategory(), c.getCost(), c.getPower(), c.getFuelType(), c.getConsumption(), "Ver más"});
+        for (final CarModel c : models) {
+            tableModel.addRow(new Object[]{c.getName(), c.getCategory(), c.getCost(), c.getPower(), c.getFuelType(), c.getConsumption(), "Ver más", "Borrar"});
         }
-        
-        Action viewMore = new AbstractAction(){
+
+        Action viewMore = new AbstractAction() {
             @Override
-            public void actionPerformed(ActionEvent e)
-            {
+            public void actionPerformed(ActionEvent e) {
                 int row = Integer.valueOf(e.getActionCommand());
                 String name = (String) tableModel.getValueAt(row, 0);
                 CarModel car = new CarModel(name);
                 CarWindow.openWindow(car);
+
             }
         };
-        ButtonColumn buttonColumn = new ButtonColumn(modelsList, viewMore, tableModel.getColumnCount() - 1);
-        buttonColumn.setMnemonic(KeyEvent.VK_D);
+
+        Action delete = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int row = Integer.valueOf(e.getActionCommand());
+                String name = (String) tableModel.getValueAt(row, 0);
+                Controller.borrar(name);
+
+            }
+        };
+        ButtonColumn columnViewMore = new ButtonColumn(modelsList, viewMore, tableModel.getColumnCount() - 2);
+        
+        ButtonColumn columnDelete = new ButtonColumn(modelsList, delete, tableModel.getColumnCount() - 1);
+        columnViewMore.setMnemonic(KeyEvent.VK_D);
+        columnDelete.setMnemonic(KeyEvent.VK_D);
     }
 }
